8edafed2e772eb04c45865e7cc12c5cdca2254d7
NotSilentNinja <47416090+NotSilentNinja@users.noreply.github.com>
2019-02-07 04:15:06 -0600

    Update AbstractRipper.java

--- src/main/java/com/rarchives/ripme/ripper/AbstractRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/AbstractRipper.java
@@ -32,7 +32,7 @@ public abstract class AbstractRipper
     private final String URLHistoryFile = Utils.getURLHistoryFile();
 
     public static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36";
 
     protected URL url;
     protected File workingDir;
